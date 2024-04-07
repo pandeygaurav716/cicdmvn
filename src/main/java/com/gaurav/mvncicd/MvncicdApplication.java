7818bae88e6dec0191c62c354c57605c8be7e343
gaurav <pandeygaurav716@gmail.com>
2024-04-08 01:57:03 +0530

    initial maven commit

---
+++ src/main/java/com/gaurav/mvncicd/MvncicdApplication.java
@@ -0,0 +1,13 @@
+package com.gaurav.mvncicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvncicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvncicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaurav/mvncicd/MvncicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.mvncicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvncicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
